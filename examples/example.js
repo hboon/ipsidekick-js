70da975de5c0144c620d203af339dfb9f0f3b10c
Hwee-Boon Yar <hboon@motionobj.com>
2017-10-16 23:30:16 +0800

    First cut

---
+++ examples/example.js
@@ -0,0 +1,45 @@
+const resolveIP = require('../lib')
+
+// Current IP address information
+resolveIP((error, json) => {
+  if (error) {
+    console.log('Error:')
+    console.log(error)
+  } else {
+    console.log('Info for current IP address:\n')
+    console.log(json)
+  }
+})
+
+// For a given IP address
+resolveIP('17.142.160.59', (error, json) => {
+  if (error) {
+    console.log('\nError:')
+    console.log(error)
+  } else {
+    console.log('\nInfo for given IP address:\n')
+    console.log(json)
+
+    // Example output:
+    // {
+    //    "ip" : "17.142.160.59",
+    //    "currency" : {
+    //       "code" : "USD",
+    //       "name" : "US dollar",
+    //       "decimals" : 2
+    //    },
+    //    "country" : {
+    //       "name" : "United States",
+    //       "code" : "US"
+    //    },
+    //    "timeZone" : {
+    //       "name" : "America/Los_Angeles",
+    //       "gmtOffset" : "GMT-7:00"
+    //    }
+    // }
+
+    console.log(`\nCountry: ${json.country.name}`)
+    console.log(`Currency: ${json.currency.name}`)
+    console.log(`Time zone: ${json.timeZone.name}\n`)
+  }
+})
